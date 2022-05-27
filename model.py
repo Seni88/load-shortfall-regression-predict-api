1609de75804676ecc1de1898ccebebf2ace7d039
Seni Ngidi <101188471+Seni88@users.noreply.github.com>
2022-05-27 17:50:56 +0200

    Update model.py

--- model.py
+++ model.py
@@ -59,28 +59,21 @@ def _preprocess_data(data):
 
     # ----------- Replace this code with your own preprocessing steps --------
 
-    # Imputing missing values with the Median
-    Valencia_pressure_Median = feature_vector_df['Valencia_pressure'].median()
-    feature_vector_df['Valencia_pressure'].fillna(Valencia_pressure_Median, inplace = True)
-
-    # Converting our 'Valencia_wind_deg' and 'Seville_pressure' features from categorical data types to numerical data types
-    # The first step is to strip away the string characters from the values
-    feature_vector_df['Valencia_wind_deg'] = feature_vector_df['Valencia_wind_deg'].str.extract('(\d+)')
-    feature_vector_df['Seville_pressure'] = feature_vector_df['Seville_pressure'].str.extract('(\d+)')
-
-    # The values' data type need to be converted to numeric, the pandas function 'to_numeric' will convert each data type
-    feature_vector_df['Valencia_wind_deg'] = pd.to_numeric(feature_vector_df['Valencia_wind_deg'])
-    feature_vector_df['Seville_pressure'] = pd.to_numeric(feature_vector_df['Seville_pressure'])
-
-    # Create new features
-    # Splitting the time feature into smaller sub-features
-    feature_vector_df['Year'] = feature_vector_df['time'].astype('datetime64').dt.year
-    feature_vector_df['Month'] = feature_vector_df['time'].astype('datetime64').dt.month
-    feature_vector_df['Day'] = feature_vector_df['time'].astype('datetime64').dt.day
-    feature_vector_df['Hour'] = feature_vector_df['time'].astype('datetime64').dt.hour
-
-    # Dropping unwanted columns
-    feature_vector_df = feature_vector_df.drop(columns=['Unnamed: 0','time'])
+    feature_vector_df = feature_vector_df[['Madrid_wind_speed',
+                                        'Bilbao_rain_1h', 'Valencia_wind_speed', 'Seville_humidity',
+                                        'Madrid_humidity', 'Bilbao_clouds_all', 'Bilbao_wind_speed',
+                                        'Seville_clouds_all', 'Bilbao_wind_deg', 'Barcelona_wind_speed',
+                                        'Barcelona_wind_deg', 'Madrid_clouds_all', 'Seville_wind_speed',
+                                        'Barcelona_rain_1h', 'Seville_rain_1h',
+                                        'Bilbao_snow_3h', 'Barcelona_pressure', 'Seville_rain_3h',
+                                        'Madrid_rain_1h', 'Barcelona_rain_3h', 'Valencia_snow_3h',
+                                        'Madrid_weather_id', 'Barcelona_weather_id', 'Bilbao_pressure',
+                                        'Seville_weather_id', 'Seville_temp_max','Madrid_pressure', 'Valencia_temp_max',
+                                        'Valencia_temp','Bilbao_weather_id', 'Seville_temp', 'Valencia_humidity',
+                                        'Valencia_temp_min', 'Barcelona_temp_max', 'Madrid_temp_max',
+                                        'Barcelona_temp', 'Bilbao_temp_min', 'Bilbao_temp',
+                                        'Barcelona_temp_min', 'Bilbao_temp_max', 'Seville_temp_min',
+                                        'Madrid_temp', 'Madrid_temp_min']]
 
     # Dropping unnecassary columns
     predict_vector = feature_vector_df
